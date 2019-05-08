b928ee30f6040a41dafe300af355d985edc99c90
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-08 17:41:01 -0400

    Se crea nueva ruta form

--- src/router.js
+++ src/router.js
@@ -36,6 +36,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "cards" */ './views/Cards.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "form" */ './views/Form.vue')
     }
   ]
 })
