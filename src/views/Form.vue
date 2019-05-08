e055df7cbe05c5d6f031dba40b1f28f4ec7c4a73
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-08 17:41:41 -0400

    Se crea un ejemplo de formulario con bootstrap-vue

---
+++ src/views/Form.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="mt-5">
+        <h1>Formularios</h1>
+        <form>
+            <b-row>
+                <b-col md="6">
+                    <!--<b-form-input type="text" v-model="text" :state="true"></b-form-input>-->
+                    <b-form-input type="text" v-model="text" :state="comprobar"></b-form-input>
+                    <small>Minimo 3 caracteres</small>
+                </b-col>
+            </b-row>
+        </form>
+        <p>text: {{text}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data () {
+    return {
+      text: ''
+    }
+  },
+  computed: {
+    comprobar () {
+      return this.text.length > 2
+    }
+  }
+}
+</script>
