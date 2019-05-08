77b6d1c4eea178bd47e30bb4975e2c65c42ef5c8
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-08 17:00:34 -0400

    Se explica funcionamiento de columnas con bootstrap-vue

---
+++ src/views/Grid.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--"mt"= marginTop-->
+  <b-row class="mt-5">
+      <!--
+          cols = es para dispositivos pequeños
+          md = para dispositivos medianos
+          lg = para dispositivos grandes
+      -->
+      <b-col cols="12" md="6" lg="4">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex est voluptates officiis officia sapiente cum quasi quae nesciunt rerum voluptate enim nam earum, quaerat molestiae veritatis soluta ipsum aspernatur vitae!</p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex est voluptates officiis officia sapiente cum quasi quae nesciunt rerum voluptate enim nam earum, quaerat molestiae veritatis soluta ipsum aspernatur vitae!</p>
+      </b-col>
+      <b-col cols="12" md="6" lg="4">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex est voluptates officiis officia sapiente cum quasi quae nesciunt rerum voluptate enim nam earum, quaerat molestiae veritatis soluta ipsum aspernatur vitae!</p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex est voluptates officiis officia sapiente cum quasi quae nesciunt rerum voluptate enim nam earum, quaerat molestiae veritatis soluta ipsum aspernatur vitae!</p>
+      </b-col>
+  </b-row>
+</template>
