b4f6da291854a6c031ffb2c2ad6229b16dfc6747
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-08 16:48:34 -0400

    Se carga un navbar funcional

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+     <!--"toggleable" indica en que tamaño de pantalla colapsara el nav-->
+     <b-navbar toggleable="md" type="dark" variant="danger">
+        <b-container>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-navbar-brand href="#">NavBar</b-navbar-brand>
+           <b-collapse id="nav-collapse" is-nav>
+             <b-navbar-nav>
+               <!--":to=" es usado para indicar que elemnto esta activo-->
+               <!--No se debe programar nada en script, la redireccion y funcionalidad la realiza solo-->
+               <b-nav-item :to="{name: 'home'}" exact>Home</b-nav-item>
+               <b-nav-item :to="{name: 'about'}">About</b-nav-item>
+             </b-navbar-nav>
+           </b-collapse>
+        </b-container>
+     </b-navbar>
+     <router-view/>
   </div>
 </template>
 
