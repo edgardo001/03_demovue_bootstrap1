ea90a7e060926db88b1f63d1aaec40bbed7a58ca
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-09 08:56:35 -0400

    Uso de campos de Formulario

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,10 +3,23 @@
         <h1>Formularios</h1>
         <form>
             <b-row>
-                <b-col md="6">
+                <b-col md="3">
                     <!--<b-form-input type="text" v-model="text" :state="true"></b-form-input>-->
                     <b-form-input type="text" v-model="text" :state="comprobar"></b-form-input>
                     <small>Minimo 3 caracteres</small>
+                </b-col>
+                <b-col md="3">
+                  <b-form-select v-model="select" :options="animales"></b-form-select>
+                  <p>seleccion: {{select}}</p>
+                </b-col>
+                <b-col md="3">
+                  <b-form-radio-group v-model="radio" :options="animalesRadio"></b-form-radio-group>
+                  <p>Radio: {{radio}}</p>
+                </b-col>
+                <b-col md="3">
+                  <b-form-checkbox-group v-model="check" :options="animalesRadio"></b-form-checkbox-group>
+                  <p>Multiple: {{check}} </p>
+                  <ul><li v-for="(item, index) in check" :key="index">{{item}}</li></ul>
                 </b-col>
             </b-row>
         </form>
@@ -19,7 +32,21 @@ export default {
   name: 'Form',
   data () {
     return {
-      text: ''
+      text: '',
+      radio: null,
+      select: null,
+      check: [],
+      animales: [
+        { value: null, text: 'Seleccione un animal' },
+        { value: 'perro', text: 'Guau guau' },
+        { value: 'gato', text: 'Miau' },
+        { value: 'pato', text: 'Cuack' }
+      ],
+      animalesRadio: [
+        { value: 'perro', text: 'Guau guau' },
+        { value: 'gato', text: 'Miau' },
+        { value: 'pato', text: 'Cuack' }
+      ]
     }
   },
   computed: {
